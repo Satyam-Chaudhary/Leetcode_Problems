98580943df7dae039c2b12d9b26227bf2020ca07
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2023-12-20 14:57:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (37.19%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int count0 = 0, count1 = 0, count2 = 0;
-        for(int i = 0 ; i < nums.size() ;i++){
-            if(nums[i] == 0) count0++;
-            if(nums[i] == 1) count1++;
-            if(nums[i] == 2) count2++;
-        }
+        // int count0 = 0, count1 = 0, count2 = 0;
+        // for(int i = 0 ; i < nums.size() ;i++){
+        //     if(nums[i] == 0) count0++;
+        //     if(nums[i] == 1) count1++;
+        //     if(nums[i] == 2) count2++;
+        // }
 
-        for(int i = 0; i < count0; i++){
-            nums[i] = 0;
-        }
-        for(int i = count0; i < count0 + count1; i++){
-            nums[i] = 1;
-        }
-        for(int i = count0 + count1; i < nums.size(); i++){
-            nums[i] = 2;
-        }
+        // for(int i = 0; i < count0; i++){
+        //     nums[i] = 0;
+        // }
+        // for(int i = count0; i < count0 + count1; i++){
+        //     nums[i] = 1;
+        // }
+        // for(int i = count0 + count1; i < nums.size(); i++){
+        //     nums[i] = 2;
+        // }
 
+        // for(auto it : nums){
+        //     cout << it << endl;
+        // }
+
+        int low = 0, mid = 0 , high = nums.size() - 1;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[low], nums[mid]);
+                mid++;
+                low++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
         for(auto it : nums){
             cout << it << endl;
         }
+
     }
 };
