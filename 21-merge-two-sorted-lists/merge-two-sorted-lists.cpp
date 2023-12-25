8f17d9a5c79d6af6fe7efd4b3f387c61a147014c
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2023-12-25 14:13:09 +0530

    Time: 7 ms (37.64%) | Memory: 15.2 MB (24.70%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,37 +11,56 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* h1, ListNode* h2) {
+        // if(h1 == NULL) return h2;
+        // if(h2 == NULL) return h1;
+        // ListNode *d = new ListNode;
+        // auto *dd = d;
+        // if(h1->val < h2->val){
+        //     dd->next = h1;
+        //     h1 = h1->next;
+        //     dd = dd->next;
+        // }else{
+        //     dd->next = h2;
+        //     h2 = h2->next;
+        //     dd = dd->next;
+        // }
+
+        // while(h1 != NULL && h2 != NULL){
+        //     if(h1->val < h2->val){
+        //         dd->next = h1;
+        //         h1 = h1->next;
+        //     }else{
+        //         dd->next = h2;
+        //         h2 = h2->next;
+        //     }
+        //     dd = dd->next;
+        // }
+
+        // if(h1 != NULL){
+        //     dd->next = h1;
+        // }else{
+        //     dd->next = h2;
+        // }
+
+        // return d->next;
+
         if(h1 == NULL) return h2;
         if(h2 == NULL) return h1;
-        ListNode *d = new ListNode;
-        auto *dd = d;
-        if(h1->val < h2->val){
-            dd->next = h1;
-            h1 = h1->next;
-            dd = dd->next;
-        }else{
-            dd->next = h2;
-            h2 = h2->next;
-            dd = dd->next;
+
+        if(h1->val > h2->val){
+            swap(h1,h2);
         }
+        ListNode *res = h1;
 
         while(h1 != NULL && h2 != NULL){
-            if(h1->val < h2->val){
-                dd->next = h1;
+            ListNode *temp = NULL;
+            while(h1 != NULL && h1->val <= h2->val){
+                temp = h1;
                 h1 = h1->next;
-            }else{
-                dd->next = h2;
-                h2 = h2->next;
             }
-            dd = dd->next;
+            temp->next = h2;
+            swap(h1,h2);
         }
-
-        if(h1 != NULL){
-            dd->next = h1;
-        }else{
-            dd->next = h2;
-        }
-
-        return d->next;
+        return res;
     }
 };
