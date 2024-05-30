24e4d98647c6fdd2a103f515732a5ae33c0cba08
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-05-30 22:16:25 +0530

    Time: 77 ms (15.94%) | Memory: 56.6 MB (86.11%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    // let maxArea = 0;
+    // for(let p1 = 0; p1 < height.length; p1++){
+    //     for(let p2 = p1+1; p2 < height.length; p2++){
+    //         const length = Math.min(height[p1], height[p2]);
+    //         const width = p2 - p1;
+    //         const area = length * width;
+    //         maxArea = Math.max(maxArea, area);
+    //     }
+    // }
+    // return maxArea;
+
+    let p1 = 0;
+    let p2 = height.length - 1;
+    let maxArea= 0;
+    while(p1 < p2){
+        let length = Math.min(height[p1], height[p2]);
+        let width = p2 - p1;
+        let area = length * width;
+        maxArea = Math.max(maxArea, area);
+
+        if(height[p1] < height[p2]){
+            p1++;
+        }else{
+            p2--;
+        }
+    }
+    return maxArea;
+};
