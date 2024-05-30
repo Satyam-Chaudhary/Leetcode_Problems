d47af02bdfb405ae27d9274c89266269466e56c5
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-05-30 11:51:41 +0530

    Time: 56 ms (80.04%) | Memory: 50 MB (57.27%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,12 +4,23 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let p1 = 0; p1 < nums.length; p1++){
-        const ntf = target - nums[p1]; //number to find
-        for(let p2 = p1 + 1; p2< nums.length; p2++){
-            if(ntf === nums[p2]){
-                return [p1,p2];
-            }
+    // for(let p1 = 0; p1 < nums.length; p1++){
+    //     const ntf = target - nums[p1]; //number to find
+    //     for(let p2 = p1 + 1; p2< nums.length; p2++){
+    //         if(ntf === nums[p2]){
+    //             return [p1,p2];
+    //         }
+    //     }
+    // }
+    // return null;
+    numsMap = {};
+    for(let p = 0; p < nums.length; p++){
+        let currentMapVal = numsMap[nums[p]];
+        if(currentMapVal >= 0){
+            return [currentMapVal, p];
+        }else{
+            const ntf = target - nums[p];
+            numsMap[ntf] = p;
         }
     }
     return null;
