b0189a886b199d56adbc94e3cb95dd62020d2912
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-06-01 20:30:16 +0530

    Time: 57 ms (45.28%) | Memory: 49.8 MB (48.95%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.js
+++ 874-backspace-string-compare/backspace-string-compare.js
@@ -4,24 +4,58 @@
  * @return {boolean}
  */
 var backspaceCompare = function(s, t) {
-   let A = checkString(s);
-   let B = checkString(t);
-  if(A === B){
+//    let A = buildString(s);
+//    let B = buildString(t);
+//   if(A === B){
+//     return true;
+//    }else{
+//     return false;
+//    }
+    let p1 = s.length - 1, p2 = t.length - 1;
+    while(p1 >= 0 || p2 >= 0){
+        if(s[p1] === '#' || t[p2] === '#'){
+            if(s[p1] === '#'){
+                let backcount = 2;
+                while(backcount > 0){
+                    p1--;
+                    backcount--;
+                    if(s[p1] === '#'){
+                        backcount += 2;
+                    }
+                }
+            }
+            if(t[p2] === '#'){
+                let backcount = 2;
+                while(backcount > 0){
+                    p2--;
+                    backcount--;
+                    if(t[p2] === '#'){
+                        backcount += 2;
+                    }
+                }
+            }
+        }else{
+            if(s[p1] !== t[p2]){
+                return false;
+            }else{
+                p1--;
+                p2--;
+            }
+        }
+    }
     return true;
-   }else{
-    return false;
-   }
+
 };
 
-function checkString(t){
-    let B = [];
-    for(let i = 0; i < t.length; i++){
-    if(t[i] !== '#'){
-        B.push(t[i]);
-    }
-    if(t[i] === '#' && t.length !== 0){
-            B.pop();
-    }
-   }
-   return B.join('');
-}
+// function buildString(t){
+//     let B = [];
+//     for(let i = 0; i < t.length; i++){
+//     if(t[i] !== '#'){
+//         B.push(t[i]);
+//     }
+//     if(t[i] === '#' && t.length !== 0){
+//             B.pop();
+//     }
+//    }
+//    return B.join('');
+// }
