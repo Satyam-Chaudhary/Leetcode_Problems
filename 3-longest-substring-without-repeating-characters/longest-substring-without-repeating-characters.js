a4c8a1421ead6b4e9d174dfc9f01258bb534a422
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-06-02 04:30:45 +0530

    Time: 104 ms (30.10%) | Memory: 55.2 MB (40.32%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -2,8 +2,8 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    if(s.length <=1) return s.length;
+var lengthOfLongestSubstring = function (s) {
+    if (s.length <= 1) return s.length;
     let longest = 0;
     // for(let p = 0; p < s.length; p++){
     //     let hm = new Map();
@@ -21,19 +21,31 @@ var lengthOfLongestSubstring = function(s) {
     // return longest;
 
 
-    for(let left = 0; left < s.length; left++){
-        let seenChar = {};
-        let cl = 0; // current length
-        for(let right = left; right < s.length; right ++){
-            const cc = [s[right]] // current Char
-            if(!seenChar[cc]){
-                cl++;
-                seenChar[cc] = true;
-                longest = Math.max(longest, cl);
-            }else{
-                break;
-            }
+    // for(let left = 0; left < s.length; left++){
+    //     let seenChar = {};
+    //     let cl = 0; // current length
+    //     for(let right = left; right < s.length; right ++){
+    //         const cc = [s[right]] // current Char
+    //         if(!seenChar[cc]){
+    //             cl++;
+    //             seenChar[cc] = true;
+    //             longest = Math.max(longest, cl);
+    //         }else{
+    //             break;
+    //         }
+    //     }
+    // }
+    // return longest;
+    let l = 0, r = 0, cl = 0;
+    let hm = {}; //hashmap
+    while (r != s.length) {
+        if (hm[s[r]] >= 0 && hm[s[r]] >= l) {
+            l = hm[s[r]] + 1;
         }
+        hm[s[r]] = r;
+        cl = (r - l) + 1;
+        longest = Math.max(cl, longest);
+        r++;
     }
     return longest;
 };
