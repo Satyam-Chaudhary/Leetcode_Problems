b4079b0b4b1642cb0886ef26f8175299e5af7292
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-06-03 03:28:17 +0530

    Time: 52 ms (94.17%) | Memory: 50.6 MB (97.53%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -6,8 +6,8 @@ var isPalindrome = function (s) {
     s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
     if(s.length === 0) return true;
     let l = 0, r = s.length -1;
-    let len = Math.ceil(s.length);
-    while(l < len){
+    // let len = Math.ceil(s.length);
+    while(l < r){
         if(s[l] === s[r]){
             l++;
             r--;
