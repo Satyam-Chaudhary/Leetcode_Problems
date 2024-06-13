c998abd5477b1ec6a24bb6e1e075cc5acec1e749
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-06-13 16:51:50 +0530

    Time: 79 ms (9.26%) | Memory: 53.1 MB (72.53%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.js
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.js
@@ -11,18 +11,39 @@
  * @return {ListNode}
  */
 var detectCycle = function(head) {
-    let cn = head;
-    const seenNodes = new Set();
+    // let cn = head;
+    // const seenNodes = new Set();
+
+    // if(!head) return null;
+
+    // while(!seenNodes.has(cn)){
+    //     if(cn.next === null){
+    //         return null;
+    //     }
+    //     seenNodes.add(cn);
+    //     cn = cn.next;
+    // }
+
+    // return cn;
+
+    // FLOYD"S HARE AND TORTOISE ALGO. FOR CYCLE DETECTION IN LL
 
     if(!head) return null;
-
-    while(!seenNodes.has(cn)){
-        if(cn.next === null){
+    let h = head, t = head; //hare and tortoise
+    while(true){
+        h = h.next;
+        t = t.next;
+        if(h === null || h.next === null){
             return null;
+        }else{
+            h = h.next;
         }
-        seenNodes.add(cn);
-        cn = cn.next;
+        if(t === h) break;
     }
-
-    return cn;
+    let p1 = head, p2 = h; // meeting point
+    while(p1 !== p2){
+        p1 = p1.next;
+        p2 = p2.next;
+    }
+    return p1;
 };
