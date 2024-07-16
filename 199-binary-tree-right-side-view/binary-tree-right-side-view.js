1213a2a8fe194d4929048416593b998d528f48d4
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-07-16 18:53:35 +0530

    Time: 65 ms (27.67%) | Memory: 50.6 MB (94.64%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.js
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.js
@@ -10,44 +10,53 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var rightSideView = function(root) {
+var rightSideView = function (root) {
     // return bfsSolution(root);
-    if(!root) return [];
+    if (!root) return [];
     let level = 0;
-    let hm = {};
     let res = [];
-    return dfsSolution(root, level, res, hm);
+    // let hm = {};
+    return dfsSolution2nd(root, level, res);
 };
 
 
 
-function dfsSolution(cn, level,  res, hm){
-    function dfsSol(cn,level){
-        if(hm[level] === undefined){
+function dfsSolution(cn, level, res, hm) {
+    function dfsSol(cn, level) {
+        if (hm[level] === undefined) { //!hm[level]
             res.push(cn.val);
-            hm[level] = cn.val;
+            hm[level] = cn.val; // hm[level] = true
         }
-        if(cn.right) dfsSol(cn.right, level+1);
-        if(cn.left) dfsSol(cn.left, level+1);
+        if (cn.right) dfsSol(cn.right, level + 1);
+        if (cn.left) dfsSol(cn.left, level + 1);
         return res;
     }
-    return dfsSol(cn,level);
+    return dfsSol(cn, level);
 }
 
 
-function bfsSolution(root){
-    if(!root) return[];
+function bfsSolution(root) {
+    if (!root) return [];
     let q = [root];
     let res = [];
-    while(q.length){
+    while (q.length) {
         let clLength = q.length;//current level length
         let cn;
-        for(let i = 0; i < clLength; i++){
+        for (let i = 0; i < clLength; i++) {
             cn = q.shift();
-            if(cn.left) q.push(cn.left);
-            if(cn.right) q.push(cn.right);
+            if (cn.left) q.push(cn.left);
+            if (cn.right) q.push(cn.right);
         }
         res.push(cn.val);
     }
+    return res;
+}
+
+function dfsSolution2nd(cn,level,res){
+    if(level >= res.length){
+        res.push(cn.val);
+    }
+    if (cn.right) dfsSolution2nd(cn.right, level + 1,res);
+    if (cn.left) dfsSolution2nd(cn.left, level + 1,res);
     return res;
 }
