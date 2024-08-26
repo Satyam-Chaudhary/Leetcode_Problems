2b60f2c2a169f2379d700143a0cebedb1d474ac5
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-08-27 02:07:03 +0530

    Time: 111 ms (35.11%) | Memory: 66.6 MB (16.33%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -0,0 +1,58 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+    // O(n2)
+    // if(nums.length <= 1) return [];
+    // let answer = new Array(nums.length);
+    // for(let i = 0; i < nums.length; i++){
+    //     let leftProduct = 1;
+    //     for(let j = 0; j < i; j++){
+    //         leftProduct = leftProduct * nums[j];
+    //     }
+    //     let rightProduct = 1;
+    //     for(let k = nums.length - 1; k > i; k--){
+    //         rightProduct = rightProduct * nums[k];
+    //     }
+    //     let product = leftProduct * rightProduct;
+    //     answer[i] = product;
+    // }
+    // return answer;
+
+    const prefix = new Array(nums.length);
+    const postfix = new Array(nums.length);
+    const answer = new Array(nums.length);
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i === 0) {
+            prefix[i] = 1 * nums[i];
+            continue;
+        }
+        prefix[i] = prefix[i - 1] * nums[i];
+    }
+
+    for (let j = nums.length - 1; j >= 0; j--) {
+        if (j === nums.length - 1) {
+            postfix[j] = 1 * nums[j];
+            continue;
+        }
+        postfix[j] = postfix[j + 1] * nums[j];
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (prefix[i - 1] === undefined) {
+            answer[i] = 1 * postfix[i + 1];
+            continue;
+        } if (postfix[i + 1] === undefined) {
+            answer[i] = prefix[i - 1] * 1;
+            continue;
+        }
+        answer[i] = prefix[i - 1] * postfix[i + 1];
+
+    }
+    console.log(prefix);
+    console.log(postfix);
+    console.log(answer);
+    return answer;
+};
