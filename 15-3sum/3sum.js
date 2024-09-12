2086360153802bc70d6125428b03366faad109a9
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-09-13 02:24:16 +0530

    Time: 170 ms (34.19%) | Memory: 65.8 MB (35.47%) - LeetSync

---
+++ 15-3sum/3sum.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+    if (nums.lenght < 3) return [];
+    const res = [];
+
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        const a = nums[i];
+        if (a > 0) break; //if 1st one is positive and the arr is sorted it would never become zero
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const threeSum = a + nums[left] + nums[right];
+            if (threeSum > 0) {
+                right--;
+            } else if (threeSum < 0) {
+                left++;
+            } else {
+                res.push([a, nums[left], nums[right]]);
+                left++;
+                right--;
+                while (left < right && nums[left] === nums[left - 1]) {
+                    left++;
+                }
+            }
+        }
+    }
+    return res;
+};
+
