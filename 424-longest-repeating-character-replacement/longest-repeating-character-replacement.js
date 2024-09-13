9d03e060d9c7996fd6012aceb46e1c87d5be86ae
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-09-14 03:36:32 +0530

    Time: 60 ms (88.64%) | Memory: 51 MB (43.13%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
@@ -13,12 +13,15 @@ function slidingWin(s,k){
     let l = 0;
     let count = new Map();
     let res = 0;
+    let maxF = 0;
 
     for(let r = 0; r < s.length ; r++){ //O(n)
         let winLen = r - l + 1;
         count.set(s[r], 1 + (count.get(s[r]) || 0));
+        maxF = Math.max(maxF , count.get(s[r]))
 
-        if(winLen - Math.max(...count.values()) > k){ //O(26)
+        // if(winLen - Math.max(...count.values()) > k){ //O(26)
+        if(winLen - maxF > k){
             count.set(s[l] , (count.get(s[l]) - 1));
             l++;
         }
