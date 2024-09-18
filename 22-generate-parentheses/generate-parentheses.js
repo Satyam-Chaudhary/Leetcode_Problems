faf481f586a271ee6a3002302c9c1e2b76901594
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-09-19 02:23:37 +0530

    Time: 54 ms (54.07%) | Memory: 51.2 MB (17.96%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.js
+++ 22-generate-parentheses/generate-parentheses.js
@@ -3,25 +3,51 @@
  * @return {string[]}
  */
 var generateParenthesis = function (n) {
+    // const res = [];
+    // function backTracking(currentString, open, close) { //DFS APPROACH
+    //     if(currentString.length === 2*n){
+    //         res.push(currentString);
+    //         return;
+    //     }
+
+    //     if(open < n){
+    //         backTracking(currentString + '(', open+1, close);
+    //     }
+
+    //     if(close < open){
+    //         backTracking(currentString + ')', open, close+1);
+    //     }
+
+    //     return; //can be omitted as it auto returned at end
+    // }
+
+    // backTracking('', 0, 0);
+    // return res;
+
+    let res = bfsSolution(n);
+    return res;
+};
+
+function bfsSolution(n){
     const res = [];
-    function backTracking(currentString, open, close) {
-        if(currentString.length === 2*n){
-            res.push(currentString);
-            return;
+    const queue = [{s:'', open: 0, close:0}];
+
+    while(queue.length > 0){
+        const {s, open, close} = queue.shift();
+
+        if(s.length === 2*n){
+            res.push(s);
+            continue;
         }
 
         if(open < n){
-            backTracking(currentString + '(', open+1, close);
+            queue.push({s: s+'(', open: open+1, close:close})
         }
 
-        if(close < open){
-            backTracking(currentString + ')', open, close+1);
+        if(close<open){
+            queue.push({s: s+')', open:open, close:close+1})
         }
-
-        return; //can be opitted as it auto returned at end
     }
-
-    backTracking('', 0, 0);
     return res;
-};
+}
 
