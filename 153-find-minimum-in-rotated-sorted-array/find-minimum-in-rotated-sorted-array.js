527a471c7b2e125b9e964d151839c896cda8ac87
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-09-30 05:34:29 +0530

    Time: 58 ms (22.26%) | Memory: 49.1 MB (37.36%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -2,40 +2,36 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMin = function(nums) {
+var findMin = function (nums) {
     //case for no rotation
     const n = nums.length;
     //case for rotation
-    return binaryApproach(nums,n);
-
+    return binaryApproach(nums, n);
 };
 
-function binaryApproach(nums,n){
+function binaryApproach(nums, n) {
     let l = 0;
-    let r = n-1;
+    let r = n - 1;
 
     let res = Infinity;
     // works only for rotated sorted array
 
-    while(l <= r){
-        let mid = Math.floor((l+r)/2);
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2);
         //sorted sub array
-        if(nums[l] < nums[r]){
+        if (nums[l] < nums[r]) {
             res = Math.min(res, nums[l]); // [4,5,1,2,3] 1 is reached before reaching sorted subarray therfore min
             break;
         }
         res = Math.min(res, nums[mid]);
         // is in sorted left subArray
-        if(nums[mid] >= nums[l]){ // = for edge case when mid on left
+        if (nums[mid] >= nums[l]) { // = for edge case when mid on left // [1] or [2,1]
             l = mid + 1;
         }
         // if in sorted right subArray
-        else{
+        else {
             r = mid - 1;
         }
     };
-
-
     return res;
-    
 };
