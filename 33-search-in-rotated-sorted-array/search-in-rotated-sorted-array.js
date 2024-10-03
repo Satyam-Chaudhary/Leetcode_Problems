feaf65b83f3265d89208ff63133ed1b1bdedcc9b
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-10-03 07:02:51 +0530

    Time: 59 ms (22.92%) | Memory: 50.5 MB (6.96%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    return searchRes(nums,target);
+};
+
+function searchRes(nums,target){
+    let pivot = findPivot(nums);
+    if(target === nums[pivot]) return pivot;
+    if(target >= nums[0] && target <= nums[pivot-1]){
+        return modifiedBinarySearch(nums,0,pivot-1,target);
+    }else{
+        return modifiedBinarySearch(nums,pivot,nums.length - 1,target);
+    }
+
+};
+
+function findPivot(nums){
+    let[l,r] = [0, nums.length - 1];
+    while(r > l){ // not l<=r as if == then infinite loop
+        let mid = Math.floor((l+r)/2);
+        if(nums[mid] > nums[r]){
+            l = mid + 1;
+        }else{
+            r = mid;
+        }
+    }
+    console.log(l);
+    return l;
+};
+
+function modifiedBinarySearch(nums,l,r,target){
+    while(l <= r){
+        let mid = Math.floor((l+r)/2);
+        if(nums[mid] === target) return mid;
+        if(nums[mid] < target){
+            l = mid + 1;
+        }else{
+            r = mid - 1;
+        }
+    }
+    return -1;
+}
