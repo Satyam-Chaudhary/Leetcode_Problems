cd970d86695d5431ff496073e21252f374c51775
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-10-03 05:35:54 +0530

    Time: 58 ms (22.03%) | Memory: 49.2 MB (24.08%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -6,32 +6,75 @@ var findMin = function (nums) {
     //case for no rotation
     const n = nums.length;
     //case for rotation
-    return binaryApproach(nums, n);
+    return findMin(nums, n);
 };
 
-function binaryApproach(nums, n) {
-    let l = 0;
-    let r = n - 1;
+// function binaryApproach(nums, n) {
+//     let l = 0;
+//     let r = n - 1;
+
+//     let res = Infinity;
+//     // works only for rotated sorted array
+
+//     while (l <= r) {
+//         let mid = Math.floor((l + r) / 2);
+//         //sorted sub array
+//         if (nums[l] < nums[r]) {
+//             res = Math.min(res, nums[l]); // [4,5,1,2,3] 1 is reached before reaching sorted subarray therfore min
+//             break;
+//         }
+//         res = Math.min(res, nums[mid]);
+//         // is in sorted left subArray
+//         if (nums[mid] >= nums[l]) { // = for edge case when mid on left // [1] or [2,1]
+//             l = mid + 1;
+//         }
+//         // if in sorted right subArray
+//         else {
+//             r = mid - 1;
+//         }
+//     };
+//     return res;
+// };
+
+
+function optimalApproach(nums,n){
+    let [l,r] = [0,n-1];
 
     let res = Infinity;
-    // works only for rotated sorted array
 
-    while (l <= r) {
-        let mid = Math.floor((l + r) / 2);
-        //sorted sub array
-        if (nums[l] < nums[r]) {
-            res = Math.min(res, nums[l]); // [4,5,1,2,3] 1 is reached before reaching sorted subarray therfore min
+    while(l <= r){
+        let mid = Math.floor((l+r)/2);
+        if(n <= 1) return nums[l]
+        if(nums[l] < nums[r]){ // sorted array or right sorted array
+            return Math.min(nums[l], res);
+            return nums[l];
             break;
         }
+
         res = Math.min(res, nums[mid]);
-        // is in sorted left subArray
-        if (nums[mid] >= nums[l]) { // = for edge case when mid on left // [1] or [2,1]
+        if(nums[mid] >= nums[l]){
             l = mid + 1;
-        }
-        // if in sorted right subArray
-        else {
+        }else{
             r = mid - 1;
         }
     };
     return res;
+}
+
+var findMin = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let pivot = (left + right) >> 1;
+
+        if (nums[pivot] < nums[nums.length - 1]) {
+            right = pivot;
+        }
+        if (nums[pivot] >= nums[nums.length -1]) {
+            left = pivot + 1;
+        }
+    }
+
+    return nums[right];
 };
