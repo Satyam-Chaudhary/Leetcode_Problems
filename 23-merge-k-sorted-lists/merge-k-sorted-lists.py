104368d77b4cb2e53c4df625fb1e9f1808be5623
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-11-03 16:55:42 +0530

    Time: 15 ms (37.13%) | Memory: 19.7 MB (40.15%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -3,14 +3,17 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        nodesArray = []
-        for list in lists:
-            while list:
-                nodesArray.append(list.val)
-                list = list.next
+        return self.heapMethod(lists)
 
+    def bruteForce(self,lists):
+        nodesArray = []
+        for l in lists:
+            while l:
+                nodesArray.append(l.val)
+                l = l.next
         nodesArray.sort()
         dummy = ListNode(0)
         curr = dummy
@@ -19,3 +22,20 @@ class Solution:
             curr = curr.next
         return dummy.next
 
+    def heapMethod(self, lists):
+        heap = []
+        for idx, l in enumerate(lists):
+            if l:
+                heapq.heappush(heap, (l.val,idx,l))
+        
+        dummy = ListNode(0)
+        curr = dummy
+        
+        while heap:
+            val,idx,node = heapq.heappop(heap)
+            curr.next = ListNode(val)
+            curr = curr.next
+            if node.next:
+                heapq.heappush(heap, (node.next.val, idx, node.next))
+
+        return dummy.next
