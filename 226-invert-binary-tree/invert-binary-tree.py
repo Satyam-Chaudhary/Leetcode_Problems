d96f0bb5a79e3ed914e53097d85a6c647d9464b5
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-11-04 23:09:09 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (13.69%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.py
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -6,7 +6,8 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        self.dfsApproach(root)
+        # self.dfsApproach(root)
+        self.bfsApproach(root)
         return root
 
     def dfsApproach(self, node):
@@ -18,3 +19,22 @@ class Solution:
         node.left = node.right
         node.right = tmp
         return
+    
+    def bfsApproach(self, node):
+        if not node:
+            return 
+        queue = deque()
+        queue.append(node)
+
+        while len(queue) > 0:
+            node = queue.popleft() 
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+            tmp = node.left
+            node.left = node.right
+            node.right = tmp
+    
+
+            
