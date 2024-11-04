ee2d792517f969dade12b6f0e0f6e539fc67cae5
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-11-04 23:00:27 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (13.69%) - LeetSync

---
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        self.dfsApproach(root)
+        return root
+
+    def dfsApproach(self, node):
+        if not node:
+            return
+        self.dfsApproach(node.left)
+        self.dfsApproach(node.right)
+        tmp = node.left
+        node.left = node.right
+        node.right = tmp
+        return
