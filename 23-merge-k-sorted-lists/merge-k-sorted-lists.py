d0623576730cbb85d6a756d66eea16bedd63b048
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-11-04 12:19:14 +0530

    Time: 12 ms (48.93%) | Memory: 19.1 MB (93.70%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -3,10 +3,10 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-import heapq
+# import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        return self.heapMethod(lists)
+        return self.divideAndCOnquer(lists)
 
     def bruteForce(self,lists):
         nodesArray = []
@@ -39,3 +39,34 @@ class Solution:
                 heapq.heappush(heap, (node.next.val, idx, node.next))
 
         return dummy.next
+    
+    def divideAndCOnquer(self, lists):
+
+        if not lists or len(lists) == 0:
+            return None
+        
+        while(len(lists) > 1):
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i+1] if (i+1) < len(lists) else None
+                mergedLists.append(self.mergeTwoLists(l1,l2))
+            lists = mergedLists
+        return lists[0]
+
+    def mergeTwoLists(self, l1, l2):
+            dummy = ListNode(0)
+            current = dummy
+            
+            while l1 and l2:
+                if l1.val <= l2.val:
+                    current.next = l1
+                    l1 = l1.next
+                else:
+                    current.next = l2
+                    l2 = l2.next
+                current = current.next
+            
+            current.next = l1 if l1 else l2
+
+            return dummy.next
