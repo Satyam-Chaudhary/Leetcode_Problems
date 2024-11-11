7080b3d429af0ffcfa499506a5388018292d4c28
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-11-11 11:37:40 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (7.59%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -11,18 +11,39 @@ class Solution:
         finalRes = []
         queue = deque([root])
         
+        def bfs(root):
 
-        while queue:
-            clLen = len(queue)
-            for i in range(clLen):
-                cn = queue.popleft()
-                if cn.left:
-                    queue.append(cn.left)
-                if cn.right:
-                    queue.append(cn.right)
+            while queue:
+                clLen = len(queue)
+                for i in range(clLen):
+                    cn = queue.popleft()
+                    if cn.left:
+                        queue.append(cn.left)
+                    if cn.right:
+                        queue.append(cn.right)
 
-                if i == clLen - 1:
-                    finalRes.append(cn.val)
+                    if i == clLen - 1:
+                        finalRes.append(cn.val)
+        def dfs(root):
+            res = []
+            def dfsSol(node, depth):
+                if not node:
+                    return
+
+                
+                if len(res) == depth:
+                    res.append(node.val)
+
+                dfsSol(node.right, depth + 1)
+                dfsSol(node.left, depth + 1)
+            
+            dfsSol(root, 0)
+            return res
+
+
+        return dfs(root)        
+        # bfs(root)
+
         
-        return finalRes
+        # return finalRes
             
