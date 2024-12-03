db6727aafde53c548844087a87a3a17b794a8ddc
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2024-12-03 19:09:27 +0530

    Time: 36 ms (93.59%) | Memory: 49.1 MB (36.87%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
@@ -1,16 +1,29 @@
 class Solution:
     def addSpaces(self, s: str, spaces: List[int]) -> str:
-        dq = deque(spaces)
-        res = []
-        li = 0 #last Index of the string 
-        for i in range(len(s)):
-            if dq and dq[0] == i:
-                res.append(s[li:i])
-                res.append(' ')
-                dq.popleft()
-                li = i
-            if i == len(s) - 1:
-                res.append(s[li:i+1])
-        
-        return ''.join(res)
 
+        def first():
+            dq = deque(spaces)
+            res = []
+            li = 0 #last Index of the string 
+            for i in range(len(s)):
+                if dq and dq[0] == i:
+                    res.append(s[li:i])
+                    res.append(' ')
+                    dq.popleft()
+                    li = i
+                if i == len(s) - 1:
+                    res.append(s[li:i+1])
+            
+            return ''.join(res)
+
+        def second():
+            li , res = 0, []
+            for space in spaces:
+                res.append(s[li:space])
+                res.append(' ')
+                li = space
+            res.append(s[li:])
+            return ''.join(res)
+
+
+        return second()
