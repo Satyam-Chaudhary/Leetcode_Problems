5458e90b15ee53ba081dadac049ca62c0f5d86fc
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2025-01-06 20:38:26 +0530

    Time: 309 ms (57.59%) | Memory: 17.8 MB (38.98%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,27 +1,27 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if not s or len(s) == 1:
-            return s  # If the string is empty or has only one character, return it as is.
+        
+        res = ""
+        resLen = 0
 
-        def expand_around_center(left, right):
-            # Expand while the substring is a palindrome
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
-            # Return the substring after expanding
-            return s[left + 1:right]
-
-        longest = ""
         for i in range(len(s)):
-            # Case 1: Odd-length palindrome (center is one character)
-            odd_palindrome = expand_around_center(i, i)
-            # Case 2: Even-length palindrome (center is between two characters)
-            even_palindrome = expand_around_center(i, i + 1)
-            
-            # Update the longest palindrome
-            if len(odd_palindrome) > len(longest):
-                longest = odd_palindrome
-            if len(even_palindrome) > len(longest):
-                longest = even_palindrome
 
-        return longest
+            #odd length substrings
+            l,r = i, i
+            while (l >= 0 and r < len(s) and s[l] == s[r]):
+                if (r - l + 1) > resLen:
+                    res = s[l: r+1]
+                    resLen = (r - l + 1)
+                l -= 1
+                r += 1
+            
+            #even Length substrings -> edge case of bb or abba
+            l,r = i, i+1
+            while (l >= 0 and r < len(s) and s[l] == s[r]):
+                if (r - l + 1) > resLen:
+                    res = s[l: r+1]
+                    resLen = (r - l + 1)
+                l -= 1
+                r += 1
+                    
+        return res
