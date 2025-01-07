9260a432a8768a78a6b96e1d5856147ef8c06540
Satyam Chaudhary <89738412+Satyam-Chaudhary@users.noreply.github.com>
2025-01-07 10:32:21 +0530

    Time: 1479 ms (36.03%) | Memory: 25.4 MB (8.95%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -38,7 +38,7 @@ class Solution:
 
             for i in range(n-1, -1, -1):
                 for j in range(i,n):
-                    if(s[i] == s[j] and (j-i <= 1 or dp[i+1][j-1])):
+                    if(s[i] == s[j] and (j-i <= 2 or dp[i+1][j-1])):
                         dp[i][j] = True
                         if(resLen < (j - i + 1)):
                             resInd = i
